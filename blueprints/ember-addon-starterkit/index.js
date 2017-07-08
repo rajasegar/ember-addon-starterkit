a899f022664d7842fca1c7fac317bbde8a8dd32d
Rajasegar Chandran <rajasegar.c@gmail.com>
2017-07-08 12:59:15 +0530

    Adding files to the blueprint for yuidoc and release configs

--- blueprints/ember-addon-starterkit/index.js
+++ blueprints/ember-addon-starterkit/index.js
@@ -1,3 +1,12 @@
+/* jshint node:true */
+var execSync = require('child_process').execSync;
+
+function runCommand(command) {
+  console.log('running: ' + command); // eslint-disable-line
+  var output = execSync(command, { encoding: 'utf8' });
+  console.log(output); // eslint-disable-line
+}
+
 /* eslint-env node */
 module.exports = {
   description: 'Starterkit for creating and publishing Ember addons',
@@ -18,6 +27,18 @@ module.exports = {
       return this.addPackagesToProject([
         {name: 'yuidoc-ember-theme'}
       ]);
+    })
+    .then(() => {
+      // Committing github-pages addon changes
+      runCommand('git add -A && git commit -m "Added ember-cli-github-pages addon"');
+
+      // Creating gh-pages branch
+      runCommand('git checkout --orphan gh-pages');
+
+      // Cleaning up branch and committing changes
+      runCommand('rm -rf `bash -c "ls -a | grep -vE \'\.gitignore|\.git|node_modules|bower_components|(^[.]{1,2}/?$)\'"` && git add -A && git commit -m "initial gh-pages commit"');
+
+      return true;
     });
   }
 };
